5f8fe945f243643f81e9aad262c7a262fd96cca5
Aegon87 <anirudhpandey8736@gmail.com>
2026-08-21 12:55:48 +0530

    Time: 20 ms (73.34%), Space: 25.8 MB (69.82%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -4,9 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        hashm = set()
-        for num in nums:
-            if num in hashm:
-                return True
-            hashm.add(num)
+        # hashm = set()
+        # for num in nums:
+        #     if num in hashm:
+        #         return True
+        #     hashm.add(num)
+        # return False
+
+        if len(nums) != len(set(nums)):
+            return True
         return False
