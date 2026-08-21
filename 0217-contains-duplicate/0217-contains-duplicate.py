4a0e9f35e917066e96c93eeade26a28d3bc95cfc
Aegon87 <anirudhpandey8736@gmail.com>
2026-08-21 12:53:35 +0530

    Time: 27 ms (56.03%), Space: 25.9 MB (69.82%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        hashm = set()
+        for num in nums:
+            if num in hashm:
+                return True
+            hashm.add(num)
+        return False
