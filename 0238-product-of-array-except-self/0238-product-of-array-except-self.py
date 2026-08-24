4a7ccf4648b27a938a3c004dee08e538f8de3c94
Aegon87 <anirudhpandey8736@gmail.com>
2026-08-24 13:57:21 +0530

    Time: 27 ms (90.65%), Space: 20.3 MB (37.09%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -15,18 +15,30 @@ class Solution(object):
         #     answer.append(prod)
         # return answer
 
-        #Better Solution
-        n = len(nums)
-        pref = [0]*n
-        suff = [0]*n
-        res = [0]*n
+        # #Better Solution
+        # n = len(nums)
+        # pref = [0]*n
+        # suff = [0]*n
+        # res = [0]*n
 
-        pref[0] = suff[n-1] = 1
-        # prefix:
-        for i in range(1, n):
-            pref[i] = pref[i-1] * nums[i-1]
-        for i in range(n-2, -1, -1):
-            suff[i] = suff[i+1] * nums[i+1]
-        for i in range(n):
-            res[i] = pref[i] * suff[i]
+        # pref[0] = suff[n-1] = 1
+        # # prefix:
+        # for i in range(1, n):
+        #     pref[i] = pref[i-1] * nums[i-1]
+        # for i in range(n-2, -1, -1):
+        #     suff[i] = suff[i+1] * nums[i+1]
+        # for i in range(n):
+        #     res[i] = pref[i] * suff[i]
+        # return res
+
+        #Optimal Solution:
+        res = [1]*len(nums)
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
         return res
