1f5891a633d12d8010d9bbacf4bb33325b6afc86
Aegon87 <anirudhpandey8736@gmail.com>
2026-08-27 18:04:05 +0530

    Time: 2 ms (90.22%), Space: 12.8 MB (46.24%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -6,10 +6,19 @@ class Solution(object):
         :rtype: List[int]
         """
         hash_ = {}
-        for i, num in enumerate(numbers):
-            
+        for i, num in enumerate(numbers): 
             compliment = target - num
             if compliment in hash_:
                 return hash_[compliment]+1, i+1
             hash_[num] = i
         return -1, -1
+
+        # l, r = 0, len(numbers)-1
+        # numbers.sort()
+        # while l < r:
+        #     if numbers[l] + numbers[r] < target:
+        #         l += 1
+        #     elif numbers[l] + numbers[r] > target:
+        #         r -= 1
+        #     else:
+        #         return l+1, r+1
