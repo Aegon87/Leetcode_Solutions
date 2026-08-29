e52e37b09026a666ad3f2bed639c552055aaa7e6
Aegon87 <anirudhpandey8736@gmail.com>
2026-08-29 16:44:58 +0530

    Time: 431 ms (21.19%), Space: 16.7 MB (27.23%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,16 +5,6 @@ class Solution(object):
         :rtype: int
         """
         res = 0
-        # l = 0
-        # st = []
-        # for r in range(len(s)):
-        #     while s[r] in st:
-        #         st.remove(s[l])
-        #         l+=1
-        #     st.append(s[r])
-        #     res = max(res, len(st))
-        # return res
-        res = 0
         l = 0
         st = set()
         for r in range(len(s)):
