f6bca616cc4cf0ef2bd2b0fa2a12785aa3d86277
Aegon87 <anirudhpandey8736@gmail.com>
2026-09-10 18:26:22 +0530

    Time: 3 ms (54.89%), Space: 20.7 MB (35.79%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,18 +1,36 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        while len(tokens) > 1:
-            for i in range(len(tokens)):
-                if tokens[i] in "+-/*":
-                    a = int(tokens[i-2])
-                    b = int(tokens[i-1])
-                    if tokens[i] == '+':
-                        result = a + b
-                    elif tokens[i] == '-':
-                        result = a - b
-                    elif tokens[i] == '*':
-                        result = a * b
-                    elif tokens[i] == '/':
-                        result = int(a / b)
-                    tokens = tokens[:i-2] + [str(result)] + tokens[i+1:]
-                    break
-        return int(tokens[0])
+        # while len(tokens) > 1:
+        #     for i in range(len(tokens)):
+        #         if tokens[i] in "+-/*":
+        #             a = int(tokens[i-2])
+        #             b = int(tokens[i-1])
+        #             if tokens[i] == '+':
+        #                 result = a + b
+        #             elif tokens[i] == '-':
+        #                 result = a - b
+        #             elif tokens[i] == '*':
+        #                 result = a * b
+        #             elif tokens[i] == '/':
+        #                 result = int(a / b)
+        #             tokens = tokens[:i-2] + [str(result)] + tokens[i+1:]
+        #             break
+        # return int(tokens[0])
+
+        stack = []
+        for i in tokens:
+            if i == "+":
+                a, b = stack.pop(), stack.pop()
+                stack.append(a + b)
+            elif i == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif i == "*":
+                a, b = stack.pop(), stack.pop()
+                stack.append(a * b)
+            elif i == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b / a)))
+            else:
+                stack.append(int(i))
+        return stack[0]
